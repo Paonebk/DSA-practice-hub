e74eb20733ebf76fdc428c60d9cfcb055c15e4ed
Paonebk018 <paone0018@gmail.com>
2026-01-17 22:39:22 +0530

    Print all odd numbers between 1 and 100.

---
+++ Basics/OddNumbers.java
@@ -0,0 +1,11 @@
+// Print all odd numbers between 1 and 100.
+public class EvenNumbers {
+    public static void main(String[] args){
+        for (int i=1;i<=99;i+=2){
+            System.out.println(i);
+//            if (i%2!=0){
+//                System.out.println(i);
+//            }
+        }
+    }
+}
