514487a27b7027868e7540135846826808c1ff3a
Paonebk018 <paone0018@gmail.com>
2026-01-23 13:15:26 +0530

    Re-written the code to find prime numbers in the range

--- Basics/Prime.java
+++ Basics/Prime.java
@@ -1,29 +1,31 @@
-//check for the prime number
-
-import java.util.Scanner;
+package org.example;
 
 public class Prime {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number");
-        int n = sc.nextInt();
 
-        if (n<=1){
-            System.out.println(n+" is not prime");//1 is not prime because it has only one divisor (1)
-        }else {
-            Boolean isPrime = true;
-            for (int i=2;i<=Math.sqrt(n);i++){
-                if (n%i==0){
-                    isPrime = false;
-                    break;
-                }
-            }
-            if (!isPrime){
-                System.out.println(n+" is not prime");
-            }else {
-                System.out.println(n+" is prime");
+    public void primeInRange(int n) {
+        for (int i = 2; i <= n; i++) {
+            if (isPrime(i)) {
+                System.out.println(i);
             }
         }
-        sc.close();
+    }
+
+    public boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+
+        int limit = (int) Math.sqrt(n);
+        for (int i = 2; i <= limit; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Prime p = new Prime();
+        p.primeInRange(10);
     }
 }
